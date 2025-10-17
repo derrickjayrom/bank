c578c0561e20d99acce7a31ebf9b385d7709291d
Dumenu Derrick <jerome.dumenu@gamil.com>
2025-10-17 22:04:14 +0000

    add request withdrawal screen

--- lib/screen/request_withdrawal.dart
+++ lib/screen/request_withdrawal.dart
@@ -12,9 +12,10 @@ class _RequestWithdrawalState extends State<RequestWithdrawal> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 56),
+            padding: EdgeInsets.only(top: 56, left: 24),
             child: Container(
               height: 44,
               width: 44,
@@ -24,6 +25,67 @@ class _RequestWithdrawalState extends State<RequestWithdrawal> {
               ),
 
               child: Icon(Icons.arrow_back_ios_new_rounded),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Request withdrawal',
+
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+                ),
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 24, left: 24, right: 24),
+                    width: double.infinity,
+                    height: 352,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFFFFFFF),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Amount to withdraw'),
+                        SizedBox(height: 8),
+                        Container(
+                          color: Colors.transparent,
+                          height: 44,
+                          width: double.infinity,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [Text('Balance:'), Text('\$9,476.00')],
+                        ),
+                        SizedBox(height: 12),
+                        Text('Country'),
+                        SizedBox(height: 8),
+                        Container(
+                          height: 44,
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey.shade400),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
